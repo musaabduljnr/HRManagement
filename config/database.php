c6eb5b3a23b815a54786896cdf0916286edf5951
musaabduljnr <musaabduljnr@gmail.com>
2026-06-16 00:33:19 +0100

    fix: remove NO_AUTO_CREATE_USER from MySQL strict modes for MySQL 8.x/9.x compatibility

--- config/database.php
+++ config/database.php
@@ -63,6 +63,14 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'strict' => true,
+            'modes' => [
+                'ONLY_FULL_GROUP_BY',
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_ENGINE_SUBSTITUTION',
+            ],
             'engine' => null,
         ],
 
